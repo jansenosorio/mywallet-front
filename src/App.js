b2deb623010583dced8f8d9f54050e32ed9d2cee
JansenOsorio <dev.jansen93@gmail.com>
2023-01-17 15:57:43 -0300

    feat: created login esctruture

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import Login from './pages/Login/Login'
+import Registration from './pages/Registration/Registration'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import styled from 'styled-components'
+
+
+const App = () => {
+
+  return (
+  <MainContainer>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={(<Login/>)} />
+        <Route path="/cadastro" element={(<Registration/>)} />
+      </Routes>
+    </BrowserRouter>
+  </MainContainer>
+  )
+}
+
+export default App
+
+const MainContainer = styled.main`
+  width: 100vw;
+  height: 100vh;
+  background-color: #9567BE;
+`
+
+
