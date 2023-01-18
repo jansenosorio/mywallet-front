6ac557d13d3855b67c5064fae8d43312bd3aab3e
JansenOsorio <dev.jansen93@gmail.com>
2023-01-17 19:15:15 -0500

    feat: includes data-tests to hub automatic correction

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,19 +1,19 @@
 import { LogoComponent } from "../../components/LogoComponent"
-import styled from "styled-components"
 import StyledInput from "../../components/StyledInput"
 import StyledLink from "../../components/StyledLink"
 import StyledButton from "../../components/StyledButton"
 import StyledForm from "../../components/StyledForm"
+import Container from '../../components/Container'
 
 const Login = () => {
   return (
     <Container>
       <LogoComponent/>
       <StyledForm>
-        <StyledInput placeholder="E-mail"/>
-        <StyledInput placeholder="Senha"/>
+        <StyledInput placeholder="E-mail" data-test="email"/>
+        <StyledInput placeholder="Senha" data-test="password"/>
       </StyledForm>
-      <StyledButton type="submit">Entrar</StyledButton>
+      <StyledButton type="submit" data-test="sign-in-submit">Entrar</StyledButton>
       <StyledLink to="/cadastro">
         Primeira vez? Cadastre-se!
       </StyledLink>
@@ -24,11 +24,3 @@ const Login = () => {
 export default Login
 
 
-const Container = styled.div`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`
